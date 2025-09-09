4c8223cda1c712e8ee7e9eddf29a1f7a6b2846de
thille6 <thille6@github.com>
2025-09-10 01:00:32 +0200

    Fix calculator import path in Routes.jsx to resolve page loading issues

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes as RouterRoutes, Route } from "react-router-dom";
 import ScrollToTop from "components/ScrollToTop";
 import ErrorBoundary from "components/ErrorBoundary";
 import NotFound from "pages/NotFound";
-import Calculator from './pages/calculator';
+import Calculator from './pages/calculator/index';
 
 const Routes = () => {
   return (
